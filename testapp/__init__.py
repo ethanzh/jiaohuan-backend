1b5f7ddda47b939e559daf8682d82dae6e6da50d
Ethan Houston <ethan.houston@gmail.com>
2016-06-19 16:02:54 +0800

    Added company

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,5 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#some_field = models.CharField(max_length=32)
-#some_field.contribute_to_class(User, 'business_card')
+some_field = models.CharField(max_length=32)
+some_field.contribute_to_class(User, 'company')
