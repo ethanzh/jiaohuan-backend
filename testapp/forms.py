1d39843baf2113f176c3ae4dc6044b095a813c14
Ethan Houston <ethan.houston@gmail.com>
2016-07-10 10:55:58 -0500

    db

--- testapp/forms.py
+++ testapp/forms.py
@@ -8,5 +8,5 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'company']
 
