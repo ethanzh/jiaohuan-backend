66c88d7ccc5f07ba5e0e5a1797e109b2853f6472
Ethan Houston <ethan.houston@gmail.com>
2016-07-10 00:35:17 -0500

    db

--- jiaohuan_backend/wsgi.py
+++ jiaohuan_backend/wsgi.py
@@ -12,5 +12,10 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'testapp.settings'
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
 application = get_wsgi_application()
--- testapp/models.py
+++ testapp/models.py
@@ -3,11 +3,13 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.conf import settings
 from django.contrib.auth.models import User
-
 from django.contrib.auth import get_user_model
+from django.db import models
 
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
+some_field = models.CharField(max_length=32)
+some_field.contribute_to_class(User, 'company')
